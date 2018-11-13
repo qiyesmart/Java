af343901ab004d2731f3b35e01aa0a493acd78d1
qiyesmart <17629009193@163.com>
2018-11-13 17:15:29 +0800

    commit lei1 file

---
+++ lei1/test5.java
@@ -0,0 +1,8 @@
+public class test5{
+		public static void main(String[] args){
+		   Person person1=new Person("阿朵","男");
+		   //person1.faceScore=15;
+		   person1.setFaceScore(15);
+		   System.out.println(person1.getPersonInfo());
+		   }
+}
