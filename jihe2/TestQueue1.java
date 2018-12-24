1bfff2a537860f8cf6b16eb9dacb7f905447f396
qiyesmart <17629009193@163.com>
2018-12-24 23:08:33 +0800

    jihe2  file

---
+++ jihe2/TestQueue1.java
@@ -0,0 +1,62 @@
+public class TestQueue1 {
+    
+    public static void code1() {
+        Queue<String> queue = new LinkedList<>();
+        ((LinkedList<String>) queue).push("A");
+        ((LinkedList<String>) queue).addLast("B");
+        queue.add("C");
+        
+        System.out.println("当前队列：");
+        for (String item : queue) {
+            System.out.println(item);
+        }
+        
+        System.out.println("进行操作：");
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());//null
+        
+        System.out.println("当前队列：");
+        for (String item : queue) {
+            System.out.println(item);
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        //队列
+        LinkedList<String> dataPool = new LinkedList<>();
+        
+        //生产者线程 1
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    dataPool.push(String.valueOf(Math.random()));
+                }
+            }
+        }).start();
+        
+        //消费者线程 1，2
+        for (int i = 0; i < 3; i++) {
+            
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        System.out.println(dataPool.poll());
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+            
+        }
+        
+    }
+}
